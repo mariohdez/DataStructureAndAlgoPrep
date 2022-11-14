c25620609b7aa9609354fb302ced7123466a9f70
Mario Hernandez <mhmario963@gmail.com>
2022-11-14 12:02:47 -0500

    add single number solution.

---
+++ DataStructureAndAlgoPrep/Week1/SingleNumberSln.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class SingleNumberSln
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int missingValue = 0;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                missingValue = missingValue ^ nums[i];
+            }
+
+            return missingValue;
+        }
+    }
+}
