e56f97dacc03a016c9dd3e9bc3d03c4826ce6663
Mario Hernandez <mhmario963@gmail.com>
2022-11-14 12:31:54 -0500

    Add missing number sln.

---
+++ DataStructureAndAlgoPrep/Week1/MissingNumberSln.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class MissingNumberSln
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int missingNumber = nums.Length;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                missingNumber = (missingNumber ^ nums[i]) ^ i;
+            }
+
+            return missingNumber;
+        }
+    }
+}
