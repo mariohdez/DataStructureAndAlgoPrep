563d01201b6b4d786a8bc4e08ea4f9d9ea411c31
Mario Hernandez <mhmario963@gmail.com>
2022-11-14 22:55:56 -0500

    Add reverse bit solution.

---
+++ DataStructureAndAlgoPrep/Week1/ReverseBits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class ReverseBits
+    {
+        public uint reverseBits(uint n)
+        {
+            uint reversedNumber = 0;
+
+            for (int i = 0; i < 32; ++i)
+            {
+                uint bit = (n >> i) & 1;
+                reversedNumber = reversedNumber | (bit << (31 - i));
+            }
+
+            return reversedNumber;
+        }
+    }
+}
