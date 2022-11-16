8942f800814ae87c486ab34a9bb41374c7fe73db
Mario Hernandez <mariohernandez@Marios-MBP.lan>
2022-11-16 00:41:46 -0500

    add move zeros.

---
+++ DataStructureAndAlgoPrep/Week1/MoveZerosSln.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class MoveZerosSln
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int lastKnownNonZero = 0;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[lastKnownNonZero];
+                    nums[lastKnownNonZero] = nums[i];
+                    nums[i] = temp;
+                    lastKnownNonZero++;
+                }
+            }
+        }
+    }
+}
+
