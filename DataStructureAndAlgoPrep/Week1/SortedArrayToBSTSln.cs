a4cbbe29fae96a7974f9528bfd375a2dc46de010
Mario Hernandez <mariohernandez@Marios-MBP.lan>
2022-11-16 01:11:59 -0500

    Ad sorted array => binary search tree.

---
+++ DataStructureAndAlgoPrep/Week1/SortedArrayToBSTSln.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+	public class SortedArrayToBSTSln
+	{
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            var root = BuildBST(nums, 0, nums.Length - 1);
+
+            return root;
+        }
+
+        public TreeNode BuildBST(int[] nums, int left, int right)
+        {
+            if (!(left <= right))
+            {
+                return null;
+            }
+
+            var mid = ((right - left) / 2) + left;
+
+            var root = new TreeNode()
+            {
+                val = nums[mid],
+            };
+
+            root.left = BuildBST(nums, left, mid - 1);
+            root.right = BuildBST(nums, mid+1, right);
+
+            return root;
+        }
+    }
+}
+
--- DataStructureAndAlgoPrep/Week1/TreeHeightBalance.cs
+++ DataStructureAndAlgoPrep/Week1/TreeHeightBalance.cs
@@ -77,6 +77,7 @@ namespace DataStructureAndAlgoPrep.Week1
     {
         public TreeNode left;
         public TreeNode right;
+        public int val;
     }
 
     public class TreeInfo
