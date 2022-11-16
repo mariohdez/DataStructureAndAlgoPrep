7b08427b8b4e79b69eb7e9d8ed0494d93c8612cb
Mario Hernandez <mariohernandez@Marios-MBP.lan>
2022-11-15 22:30:39 -0500

    add LCP.

---
+++ DataStructureAndAlgoPrep/Week1/LongestCommonPrefixSln.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class LongestCommonPrefixSln
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            for (int i = 0; i < strs[0].Length; ++i)
+            {
+                char currentCharacter = strs[0][i];
+
+                for (int j = 1; j < strs.Length; ++j)
+                {
+                    if (i >= strs[j].Length || strs[j][i] != currentCharacter)
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+            }
+
+            return strs[0];
+        }
+    }
+}
+
