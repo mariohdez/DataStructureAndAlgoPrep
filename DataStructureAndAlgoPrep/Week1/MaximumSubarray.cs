3087e715c4c1fa67357ef872be4d98caa3c56fb8
Mario Hernandez <mhmario963@gmail.com>
2022-11-18 20:41:52 -0700

    add max subarray sln.

---
+++ DataStructureAndAlgoPrep/Week1/MaximumSubarray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week1
+{
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            int max = dp[0];
+
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                dp[i] = Math.Max(dp[i - 1] + nums[i], nums[i]);
+                max = Math.Max(max, dp[i]);
+            }
+            return max;
+        }
+
+    }
+}
