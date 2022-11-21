679ae163b04f4b5adad7a10f760ac75f1f6b6f26
Mario Hernandez <mhmario963@gmail.com>
2022-11-20 20:58:20 -0700

    add clone graph.

---
+++ DataStructureAndAlgoPrep/Week2/CloneGraphSln.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week2
+{
+    public class CloneGraphSln
+    {
+        Dictionary<Node, Node> visitedNodesToCreatedNodeCopy = new Dictionary<Node, Node>();
+
+        public Node CloneGraph(Node node)
+        {
+            if (node == null) return null;
+
+            if (visitedNodesToCreatedNodeCopy.ContainsKey(node))
+            {
+                return visitedNodesToCreatedNodeCopy[node];
+            }
+
+
+            Node cloneNode = new Node(node.val, new List<Node>());
+
+            visitedNodesToCreatedNodeCopy.Add(node, cloneNode);
+
+            for (int i = 0; i < node.neighbors.Count; ++i)
+            {
+                cloneNode.neighbors.Add(CloneGraph(node.neighbors[i]));
+            }
+
+            return cloneNode;
+        }
+    }
+}
---
+++ DataStructureAndAlgoPrep/Week2/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week2
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
+        {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
