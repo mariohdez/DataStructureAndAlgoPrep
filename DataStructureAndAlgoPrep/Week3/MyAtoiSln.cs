e2ab245cec6aeed0680aec46bc798893fe0801ee
Mario Hernandez <mhmario963@gmail.com>
2022-11-25 15:19:43 -0700

    add atoi sln

---
+++ DataStructureAndAlgoPrep/Week3/MyAtoiSln.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week3
+{
+    public class MyAtoiSln
+    {
+        public int MyAtoi(string s)
+        {
+            int integer = 0;
+            int i = 0;
+            int len = s.Length;
+            bool negative = false;
+            int digit = 0;
+
+            while (i < len && char.IsWhiteSpace(s[i])) i++;
+
+            if (i < len && (s[i] == '+' || s[i] == '-'))
+            {
+                negative = s[i] == '-';
+                i++;
+            }
+
+            while (i < len && char.IsDigit(s[i]))
+            {
+                digit = s[i] - '0';
+
+                if ((integer > int.MaxValue / 10) ||
+                    (integer == int.MaxValue / 10 && digit > int.MaxValue % 10))
+                {
+                    return negative ? int.MinValue : int.MaxValue;
+                }
+
+                integer = (integer * 10) + digit;
+                i++;
+            }
+
+            return negative ? integer * -1 : integer;
+        }
+    }
+}
