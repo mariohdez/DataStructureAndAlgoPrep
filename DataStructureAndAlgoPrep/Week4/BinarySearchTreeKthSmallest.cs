6be094d628fa6f6bc10d9bf98c312f95e8045dcb
Mario Hernandez <mhmario963@gmail.com>
2022-11-30 13:41:49 -0700

    Add k smallest bst.

---
+++ DataStructureAndAlgoPrep/Week4/BinarySearchTreeKthSmallest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DataStructureAndAlgoPrep.Week1;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week4
+{
+    public class BinarySearchTreeKthSmallest
+    {
+        private readonly IList<int> inOrderTraversalList = new List<int>();
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            InOrderTraversal(root);
+
+            return inOrderTraversalList[k - 1];
+        }
+
+        public void InOrderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            InOrderTraversal(root.left);
+            this.inOrderTraversalList.Add(root.val);
+            InOrderTraversal(root.right);
+        }
+    }
+}
