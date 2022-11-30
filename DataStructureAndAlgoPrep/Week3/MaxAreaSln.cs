121cdd1e54d1a28ee42df4564c5eb05bc92d0c3d
Mario Hernandez <mhmario963@gmail.com>
2022-11-29 19:21:06 -0700

    Add sln for max area (container with most water).

---
+++ DataStructureAndAlgoPrep/Week3/MaxAreaSln.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week3
+{
+    public class MaxAreaSln
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = int.MinValue;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int width = right - left;
+                int curHeight = Math.Min(height[left], height[right]);
+                int currentArea = width * curHeight;
+
+                if (currentArea > maxArea)
+                {
+                    maxArea = currentArea;
+                }
+
+                if (height[left] > height[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
