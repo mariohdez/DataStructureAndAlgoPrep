6c9d165e75d36cf6d66ee28d871fd1f1eb7da537
Mario Hernandez <mhmario963@gmail.com>
2022-11-29 19:46:13 -0700

    Add word search sln.

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DataStructureAndAlgoPrep.Week1;
 using DataStructureAndAlgoPrep.Week2;
 using DataStructureAndAlgoPrep.Week3;
+using DataStructureAndAlgoPrep.Week4;
 
 namespace DataStructureAndAlgoPrep
 {
@@ -10,24 +11,16 @@ namespace DataStructureAndAlgoPrep
     {
         static void Main(string[] args)
         {
-            var test = new SpiralOrderSln();
-            var matrix = new int[3][]
+            var test = new WordSearchSln();
+            var matrix = new char[3][]
             {
-                new int[3]{ 1, 2, 3 },
-                new int[3]{ 4, 5, 6 },
-                new int[3]{ 7, 8, 9 },
+                new char[4]{ 'a', 'b', 'c', 'e' },
+                new char[4]{ 's', 'f', 'c', 's' },
+                new char[4]{ 'a', 'd', 'e', 'e' },
             };
-            var res = test.SpiralOrder(matrix);
+            var res = test.Exist(matrix, "abcced");
 
             System.Console.WriteLine(res);
         }
     }
 }
-
-
-
-/**
- * 
-[["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
- * 
- */
---
+++ DataStructureAndAlgoPrep/Week4/WordSearchSln.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week4
+{
+    public class WordSearchSln
+    {
+        private int[][] directions = new int[4][]
+        {
+            new int[2] { -1,  0 },
+            new int[2] {  1,  0 },
+            new int[2] {  0, -1 },
+            new int[2] {  0,  1 },
+        };
+
+        public bool Exist(char[][] board, string word)
+        {
+            for (int i = 0; i < board.Length; ++i)
+            {
+                for (int j = 0; j < board[0].Length; ++j)
+                {
+                    if (board[i][j] == word[0] && DFS(board, word, 0, i, j))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool DFS(char[][] board, string word, int currentWordIndex, int currentRow, int currentCol)
+        {
+            // We found the word.
+            if (currentWordIndex >= word.Length)
+            {
+                return true;
+            }
+
+            // Out of bounds.
+            if (currentRow < 0 || currentRow >= board.Length || currentCol < 0 || currentCol >= board[0].Length)
+            {
+                return false;
+            }
+
+            // We have already visted this item.
+            if (board[currentRow][currentCol] == '#')
+            {
+                return false;
+            }
+
+            if (board[currentRow][currentCol] != word[currentWordIndex])
+            {
+                return false;
+            }
+
+            // mark this square as visited.
+            char previousChar = board[currentRow][currentCol];
+            board[currentRow][currentCol] = '#';
+            var exists = false;
+            foreach (var direction in this.directions)
+            {
+                exists = exists || DFS(
+                    board: board,
+                    word: word,
+                    currentWordIndex: currentWordIndex + 1,
+                    currentRow: currentRow + direction[0],
+                    currentCol: currentCol + direction[1]);
+            }
+
+            board[currentRow][currentCol] = previousChar;
+
+            return exists;
+        }
+    }
+}
