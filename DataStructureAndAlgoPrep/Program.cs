cbcadbf49326762aedd7dfd594ecf1a702f564e5
Mario Hernandez <mhmario963@gmail.com>
2022-12-09 21:46:27 -0700

    Add zig zag level order.

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -12,33 +12,19 @@ namespace DataStructureAndAlgoPrep
     {
         static void Main(string[] args)
         {
-            var board = new char[][] {
-                new char[] { 'a' },
-            };
+            var root = new TreeNode(3);
+            var nine = new TreeNode(9);
+            var twenty = new TreeNode(20);
+            var fifteen = new TreeNode(15);
+            var seven = new TreeNode(7);
 
-            /*
-             * 
-             * 
-             
-             * 
-             * int n, int[][] flights, int src, int dst, int k
-             n = 3, flights = [[],[],[]], src = 0, dst = 2, k = 1
+            root.left = nine;
+            root.right = twenty;
+            twenty.left = fifteen;
+            twenty.right = seven;
 
-             */
-
-            var test = new FindCheapestPriceSln();
-            var res = test.FindCheapestPrice(
-                n: 3,
-                flights: new int[][]
-                {
-                    new int[] { 0,1,100 },
-                    new int[] { 1,2,100 },
-                    new int[] { 0,2,500 },
-                },
-                src: 0,
-                dst: 2,
-                k: 1);
-
+            var test = new ZigzagLevelOrderSln();
+            var res = test.ZigzagLevelOrder(root);
 
             Console.WriteLine(res);
         }
---
+++ DataStructureAndAlgoPrep/Week5/ZigzagLevelOrderSln.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DataStructureAndAlgoPrep.Week1;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week5
+{
+    public class ZigzagLevelOrderSln
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            int zigZagCounter = 0;
+            int numberOfNodesOnLevel = 0;
+            List<IList<int>> answer = new List<IList<int>>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            int[] levelAnswer;
+            int levelAnswerIndex = 0;
+
+            if (root == null) return answer;
+
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                numberOfNodesOnLevel = queue.Count;
+                levelAnswer = new int[numberOfNodesOnLevel];
+                bool isZigZagEven = zigZagCounter % 2 == 0;
+
+                for (int i = 0; i < numberOfNodesOnLevel; ++i)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    levelAnswerIndex = isZigZagEven ? i : (numberOfNodesOnLevel - 1) - i;
+                    levelAnswer[levelAnswerIndex] = currentNode.val;
+
+                    if (currentNode.left != null)
+                    {
+                        queue.Enqueue(currentNode.left);
+                    }
+
+                    if (currentNode.right != null)
+                    {
+                        queue.Enqueue(currentNode.right);
+                    }
+                }
+
+                answer.Add(levelAnswer);
+                zigZagCounter++;
+            }
+
+            return answer;
+        }
+    }
+}
