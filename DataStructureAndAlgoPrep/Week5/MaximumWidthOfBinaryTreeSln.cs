825da3f81ecbcaf8ba99b39ca258707ca05d8648
Mario Hernandez <mhmario963@gmail.com>
2022-12-09 23:03:29 -0700

    Add max width of binary tree sln.

---
+++ DataStructureAndAlgoPrep/Week5/MaximumWidthOfBinaryTreeSln.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructureAndAlgoPrep.Week1;
+
+namespace DataStructureAndAlgoPrep.Week5
+{
+    public class MaximumWidthOfBinaryTreeSln
+    {
+        public int WidthOfBinaryTree(TreeNode root)
+        {
+            int maxWidth = int.MinValue;
+            int numberOfNodesInLevel = 0;
+            Queue<(TreeNode node, int index)> queue = new Queue<(TreeNode node, int index)>();
+
+            if (root == null) return 0;
+
+            queue.Enqueue((node: root, index: 1));
+
+            (TreeNode node, int index) current = (null, 0);
+            while (queue.Count > 0)
+            {
+                numberOfNodesInLevel = queue.Count;
+                (TreeNode head, int headIndex) head = queue.Peek();
+                for (int i = 0; i < numberOfNodesInLevel; ++i)
+                {
+                    current = queue.Dequeue();
+
+                    if (current.node.left != null)
+                    {
+                        queue.Enqueue((current.node.left, 2 * current.index));
+                    }
+
+                    if (current.node.right != null)
+                    {
+                        queue.Enqueue((current.node.right, (2 * current.index) + 1));
+                    }
+                }
+                maxWidth = Math.Max(maxWidth, current.index - head.headIndex + 1);
+            }
+
+            return maxWidth;
+        }
+    }
+}
