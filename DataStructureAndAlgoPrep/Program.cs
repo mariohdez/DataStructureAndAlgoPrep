149e7febcd679fa0deca46f2a47a4c2520ef9e42
Mario Hernandez <mhmario963@gmail.com>
2022-12-09 23:32:49 -0700

    add distance k sln

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -13,18 +13,26 @@ namespace DataStructureAndAlgoPrep
         static void Main(string[] args)
         {
             var root = new TreeNode(3);
-            var nine = new TreeNode(9);
-            var twenty = new TreeNode(20);
-            var fifteen = new TreeNode(15);
+            var five = new TreeNode(5);
+            var one = new TreeNode(1);
+            var six = new TreeNode(6);
+            var two = new TreeNode(2);
+            var zero = new TreeNode(0);
+            var eight = new TreeNode(8);
             var seven = new TreeNode(7);
+            var four = new TreeNode(4);
+            root.left = five;
+            root.right = one;
+            five.left = six;
+            five.right = two;
+            one.left = zero;
+            one.right = eight;
+            two.left = seven;
+            two.right = four;
 
-            root.left = nine;
-            root.right = twenty;
-            twenty.left = fifteen;
-            twenty.right = seven;
 
-            var test = new ZigzagLevelOrderSln();
-            var res = test.ZigzagLevelOrder(root);
+            var test = new DistanceKSln();
+            var res = test.DistanceK(root, target: five, k: 2);
 
             Console.WriteLine(res);
         }
---
+++ DataStructureAndAlgoPrep/Week5/DistanceKSln.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using DataStructureAndAlgoPrep.Week1;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week5
+{
+    public class DistanceKSln
+    {
+        private IDictionary<TreeNode, IList<TreeNode>> adjacencyGraph = new Dictionary<TreeNode, IList<TreeNode>>();
+
+        public IList<int> DistanceK(TreeNode root, TreeNode target, int k)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            ISet<TreeNode> visited = new HashSet<TreeNode>();
+            IList<int> result = new List<int>();
+            int numberOfNodesInLevel = 0;
+            int distance = 0;
+            BuildGraph(root, null);
+
+            queue.Enqueue(target);
+            visited.Add(target);
+
+
+            while (queue.Count > 0 && distance <= k)
+            {
+                numberOfNodesInLevel = queue.Count;
+
+                for (int i = 0; i < numberOfNodesInLevel; ++i)
+                {
+                    TreeNode node = queue.Dequeue();
+
+                    if (distance == k)
+                    {
+                        result.Add(node.val);
+                    }
+
+                    foreach (TreeNode neighbor in adjacencyGraph[node])
+                    {
+                        if (visited.Contains(neighbor))
+                        {
+                            continue;
+                        }
+
+                        visited.Add(neighbor);
+                        queue.Enqueue(neighbor);
+                    }
+                }
+
+                distance++;
+            }
+
+            return result;
+        }
+
+        public void BuildGraph(TreeNode currentNode, TreeNode parent)
+        {
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            if (!this.adjacencyGraph.ContainsKey(currentNode))
+            {
+                this.adjacencyGraph.Add(currentNode, new List<TreeNode>());
+            }
+
+            if (parent != null)
+            {
+                this.adjacencyGraph[currentNode].Add(parent);
+
+                if (!this.adjacencyGraph.ContainsKey(parent))
+                {
+                    this.adjacencyGraph.Add(parent, new List<TreeNode>());
+                }
+
+                this.adjacencyGraph[parent].Add(currentNode);
+            }
+
+            BuildGraph(currentNode.left, currentNode);
+            BuildGraph(currentNode.right, currentNode);
+        }
+    }
+}
