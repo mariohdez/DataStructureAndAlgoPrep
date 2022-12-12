2f5d988fe07e9a21f6471380a43097ef4553f496
Mario Hernandez <mhmario963@gmail.com>
2022-12-11 17:56:44 -0700

    Add Subarray Sum Equals K sln.

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -12,27 +12,16 @@ namespace DataStructureAndAlgoPrep
     {
         static void Main(string[] args)
         {
-            var root = new TreeNode(3);
-            var five = new TreeNode(5);
-            var one = new TreeNode(1);
-            var six = new TreeNode(6);
-            var two = new TreeNode(2);
-            var zero = new TreeNode(0);
-            var eight = new TreeNode(8);
-            var seven = new TreeNode(7);
-            var four = new TreeNode(4);
-            root.left = five;
-            root.right = one;
-            five.left = six;
-            five.right = two;
-            one.left = zero;
-            one.right = eight;
-            two.left = seven;
-            two.right = four;
+            /*
+             nums = [1,2,3], k = 3
+             
+             */
+            int[] nums = new int[] { 1, 2, 3 };
+            int k = 3;
 
 
-            var test = new DistanceKSln();
-            var res = test.DistanceK(root, target: five, k: 2);
+            var test = new SubarraySumSln();
+            var res = test.SubarraySum(nums, k: k);
 
             Console.WriteLine(res);
         }
---
+++ DataStructureAndAlgoPrep/Week5/SubarraySumSln.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureAndAlgoPrep.Week5
+{
+    public class SubarraySumSln
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            int answer = 0;
+            IDictionary<int, int> prefixSumToFrequencyMap = new Dictionary<int, int>();
+            prefixSumToFrequencyMap.Add(0, 1);
+            int prefixSum = 0;
+            int targetValue = 0;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                prefixSum += nums[i];
+                targetValue = prefixSum - k;
+                if (prefixSumToFrequencyMap.ContainsKey(targetValue))
+                {
+                    answer += prefixSumToFrequencyMap[targetValue];
+                }
+
+                if (!prefixSumToFrequencyMap.ContainsKey(prefixSum))
+                {
+                    prefixSumToFrequencyMap.Add(prefixSum, 0);
+                }
+
+                prefixSumToFrequencyMap[prefixSum] += 1;
+            }
+
+            return answer;
+        }
+    }
+}
