a6cfa45cb5e14718949eeaef6a715ed6d465730b
Mario Hernandez <mhmario963@gmail.com>
2022-12-12 12:22:09 -0700

    Add next permutation sln.

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -5,6 +5,7 @@ using DataStructureAndAlgoPrep.Week2;
 using DataStructureAndAlgoPrep.Week3;
 using DataStructureAndAlgoPrep.Week4;
 using DataStructureAndAlgoPrep.Week5;
+using DataStructureAndAlgoPrep.Week6;
 
 namespace DataStructureAndAlgoPrep
 {
@@ -12,25 +13,12 @@ namespace DataStructureAndAlgoPrep
     {
         static void Main(string[] args)
         {
-            // [,null,1000000000,null,1000000000,null,1000000000]
+            // 9, 5, 4, 3, 2, 1
+            // 1, 3, 5, 4, 3, 2, 1
+            int[] nums = new int[] { 1, 2, 3 };
 
-            TreeNode root = new TreeNode(1000000000);
-            TreeNode left1 = new TreeNode(1000000000);
-            TreeNode left2 = new TreeNode(294967296);
-            TreeNode left3 = new TreeNode(1000000000);
-            TreeNode left4 = new TreeNode(1000000000);
-            TreeNode left5 = new TreeNode(1000000000);
-
-            root.left = left1;
-            left1.left = left2;
-            left2.left = left3;
-            left3.left = left4;
-
-
-            var test = new PathSumIIISln();
-            var res = test.PathSum(root: root, targetSum: 0);
-
-            Console.WriteLine(res);
+            var test = new NextPermutationSln();
+            test.NextPermutation(nums: nums);
         }
     }
 }
---
+++ DataStructureAndAlgoPrep/Week6/NextPermutationSln.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace DataStructureAndAlgoPrep.Week6
+{
+    public class NextPermutationSln
+    {
+        public void NextPermutation(int[] nums)
+        {
+            if (nums == null) return;
+
+            int pivot = nums.Length-1;
+            int i = nums.Length - 1;
+
+            while (pivot >= 1 && nums[pivot] <= nums[pivot - 1])
+            {
+                pivot--;
+            }
+
+            int indexOfValueLeftOfPeak = pivot - 1;
+            int swapIndex = nums.Length - 1;
+
+            if (pivot > 0)
+            {
+                while (indexOfValueLeftOfPeak >= 0 && nums[indexOfValueLeftOfPeak] >= nums[swapIndex])
+                {
+                    swapIndex--;
+                }
+
+                int temp = nums[pivot - 1];
+                nums[pivot - 1] = nums[swapIndex];
+                nums[swapIndex] = temp;
+            }
+
+            Reverse(nums, pivot);
+        }
+
+        public void Reverse(int[] nums, int start)
+        {
+            int end = nums.Length - 1;
+            int temp = 0;
+
+            while (start < end)
+            {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+    }
+}
