ee09b9fae3333b3fffc331f8e9ddbb929d8f987d
Mario Hernandez <mhmario963@gmail.com>
2022-12-13 14:23:21 -0700

    Add swap pairs sln.

--- DataStructureAndAlgoPrep/Program.cs
+++ DataStructureAndAlgoPrep/Program.cs
@@ -14,18 +14,16 @@ namespace DataStructureAndAlgoPrep
         static void Main(string[] args)
         {
             ListNode one = new ListNode(1);
-            //ListNode two = new ListNode(2);
-            //ListNode three = new ListNode(3);
-            //ListNode four = new ListNode(4);
-            //ListNode five = new ListNode(5);
-            //one.next = two;
-            //two.next = three;
-            //three.next = four;
-            //four.next = five;
+            ListNode two = new ListNode(2);
+            ListNode three = new ListNode(3);
+            ListNode four = new ListNode(4);
+            one.next = two;
+            two.next = three;
+            three.next = four;
+            
 
-            int n = 1;
-            var test = new RemoveNthFromEndSln();
-            test.RemoveNthFromEnd(one, n);
+            var test = new SwapPairsSln();
+            test.SwapPairs(one);
         }
     }
 }
---
+++ DataStructureAndAlgoPrep/Week6/SwapPairsSln.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using DataStructureAndAlgoPrep.Week1;
+
+namespace DataStructureAndAlgoPrep.Week6
+{
+    public class SwapPairsSln
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode current = head;
+            ListNode neighbor = current.next;
+            ListNode newHead = neighbor;
+
+            if (neighbor == null)
+            {
+                return head;
+            }
+
+            while (current != null && neighbor != null)
+            {
+                current.next = neighbor.next;
+                neighbor.next = current;
+                current = current.next;
+                if (current!=null)
+                {
+                    neighbor = current.next;
+                }
+            }
+
+            return newHead;
+        }
+    }
+}
