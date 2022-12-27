d2bc8cbd1f0dd14eb2ee25986ca53122eca252bf
Mario Hernandez <mhmario963@gmail.com>
2022-12-27 14:13:37 -0700

    Add binary tree max path sum solution.

---
+++ DataStructureAndAlgoPrep/Week8/MaxPathSumSln.cs
@@ -0,0 +1,30 @@
+
+using System;
+using DataStructureAndAlgoPrep.Week1;
+
+namespace DataStructureAndAlgoPrep.Week8;
+
+public class MaxPathSumSln
+{
+    private int maxSum = int.MinValue;
+
+    public int MaxPathSum(TreeNode root)
+    {
+
+    }
+
+    public int MaxPathSumHelper(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int left = Math.Max(MaxPathSumHelper(root.left), 0);
+        int right = Math.Max(MaxPathSumHelper(root.right), 0);
+
+        this.maxSum = Math.Max(this.maxSum, left + right + root.val);
+
+        return Math.Max(left + root.val, right + root.val);
+    }
+}
